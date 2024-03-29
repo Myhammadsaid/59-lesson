f5f78b722c5c40353bd475a60d21648e092f0ff7
Mуhammadsaid <muhammadsaidmm4@gmail.com>
2024-03-29 13:54:26 +0300

    appdat

--- js/wishes.js
+++ js/wishes.js
@@ -44,3 +44,12 @@ wrapper.addEventListener("click", (e) => {
     removeWishes(id);
   }
 });
+
+// ============= Navbar toggle START ================
+const navbarCollection = document.querySelector(".navbar__collection");
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", () => {
+  navbarCollection.classList.toggle("show");
+});
+// ============= Navbar toggle END ================
